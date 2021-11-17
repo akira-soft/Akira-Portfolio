eb06d2807c719bba4db132ec4891ba6a53b10437
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-11-17 00:19:33 -0600

    Added a toolbar with the system routing and the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponentComponent } from './pages/page-not-found-component/page-not-found-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+const MODULES = [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
+];
+
+@NgModule({
+    imports: [
+        ...MODULES
+    ],
+    exports: [
+        ...MODULES
+    ]
+})
+export class MaterialModule { }
